6fe89f2b7651ce9213377d78ca49639f92a8f1cd
Daniil Grishin <ddg071997@gmail.com>
2023-01-15 18:42:27 +0300

    Added Task004 Solution

---
+++ Homework/HW001-15-01-23/Task004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Enter First Number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Third Number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int initialMax = Math.Max(num1, num2);
+int finalMax = Math.Max(initialMax, num3);
+
+Console.Write("max = ");
+Console.WriteLine(finalMax);
