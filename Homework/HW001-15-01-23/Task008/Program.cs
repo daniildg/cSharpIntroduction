c2cf268e1730b356079d8b404f0bf15d99e6ebbc
Daniil Grishin <ddg071997@gmail.com>
2023-01-15 19:27:21 +0300

    Added Task008 Solution

---
+++ Homework/HW001-15-01-23/Task008/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+System.Console.WriteLine("Enter Number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+  Console.Write(i);
+  Console.Write(" ");
+}
