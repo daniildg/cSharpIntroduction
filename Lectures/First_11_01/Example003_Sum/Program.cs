51818e584e487a8fb63a05ef07c81d5e8e9f4e05
Daniil Grishin <ddg071997@gmail.com>
2023-01-17 15:53:58 +0300

    Updated Example003 Random Numbers Sum Project

--- Lectures/First_11_01/Example003_Sum/Program.cs
+++ Lectures/First_11_01/Example003_Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 2356;
-int numberB = 7851;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4...9
+Console.Write(numberA);
+Console.Write(" + ");
+int numberB = new Random().Next(1, 10);
+Console.Write(numberB);
 int result = numberA + numberB;
+Console.Write(" = ");
 Console.WriteLine(result);
