d409b212fbbd8b4ef149b24f64a0d1f2afaa5910
Daniil Grishin <ddg071997@gmail.com>
2023-01-17 19:16:27 +0300

    Added Task001 Square Solution

---
+++ Seminars/First_13_01/Task001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.Write("Enter First Number -> ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Second Number -> ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int square = number2 * number2;
+
+if(number1 == square)
+{
+  System.Console.WriteLine("True");
+}
+else
+{
+  System.Console.WriteLine("False");
+}
