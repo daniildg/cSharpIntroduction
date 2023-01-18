64408dff3bb540bd1675459a19255252ed43379f
Daniil Grishin <ddg071997@gmail.com>
2023-01-18 16:30:09 +0300

    Added Task010 Solution

---
+++ Homework/HW002-18-01-23/Task010/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Enter Number ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num} -> {(num % 100) / 10}");
