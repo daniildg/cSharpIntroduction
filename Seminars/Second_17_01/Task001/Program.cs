738886e474bebfe656c8c14f6e3d236af8d4a113
Daniil Grishin <ddg071997@gmail.com>
2023-01-18 10:52:48 +0300

    Added Task001 Solution

---
+++ Seminars/Second_17_01/Task001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая выодит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру этого числа.
+
+int num = new Random().Next(10, 100);
+System.Console.WriteLine($"{"Your Number Is"} -> {num}");
+int firstResult = num / 10;
+int secondResult = num % 10;
+
+if(firstResult < secondResult)
+{
+  System.Console.WriteLine($"{"Largest Number Is"} -> {secondResult}");
+}
+else
+{
+  System.Console.WriteLine($"{"Largest Number Is"} -> {firstResult}");
+}
