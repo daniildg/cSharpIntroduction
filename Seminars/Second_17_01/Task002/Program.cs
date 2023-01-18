5bd77e0ea7c63aadcba3a489fca155c26adda0c1
Daniil Grishin <ddg071997@gmail.com>
2023-01-18 11:32:10 +0300

    Update №2 Task002 Solution

--- Seminars/Second_17_01/Task002/Program.cs
+++ Seminars/Second_17_01/Task002/Program.cs
@@ -2,6 +2,5 @@
 // и удаляет вторую цифру числа.
 
 int num = new Random().Next(100, 1000);
-System.Console.WriteLine($"Number -> {num}");
 int result = (num / 100) * 10 + num % 10;
-System.Console.WriteLine($"Result -> {result}");
+System.Console.WriteLine($"{num} -> {result}");
