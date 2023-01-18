380a58119f402adc30ee83ed2fd40caf669abca6
Daniil Grishin <ddg071997@gmail.com>
2023-01-18 11:16:38 +0300

    Added Task002 Solution

---
+++ Seminars/Second_17_01/Task002/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа, которая выводит случайное трехзначное число
+// и удаляет вторую цифру числа.
+
+int num = new Random().Next(100, 1000);
+System.Console.WriteLine($"{"Number"} -> {num}");
+int result = (num / 100) * 10 + num % 10;
+System.Console.WriteLine($"{"Result"} -> {result}");
