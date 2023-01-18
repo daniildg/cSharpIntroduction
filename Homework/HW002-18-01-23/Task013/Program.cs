91d70e0a11a88878a12038184618f9fb5601d50e
Daniil Grishin <ddg071997@gmail.com>
2023-01-18 16:18:09 +0300

    Added Task013 Solution

---
+++ Homework/HW002-18-01-23/Task013/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+if(num > 99)
+{
+  while (num / 1000 != 0)
+  {
+    num = num / 10;
+  }
+  System.Console.WriteLine(num % 10);
+}
+else
+{
+  System.Console.WriteLine("третьей цифры нет");
+}
