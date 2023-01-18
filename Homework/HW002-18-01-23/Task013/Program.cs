5a670658e0f85b77d19b2bcc5282f47988448ea5
Daniil Grishin <ddg071997@gmail.com>
2023-01-18 17:04:19 +0300

    Updated Task013 Solution

--- Homework/HW002-18-01-23/Task013/Program.cs
+++ Homework/HW002-18-01-23/Task013/Program.cs
@@ -1,13 +1,18 @@
-﻿int num = Convert.ToInt32(Console.ReadLine());
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+System.Console.Write("Enter Number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int initialNumber = num;
 if(num > 99)
 {
   while (num / 1000 != 0)
   {
     num = num / 10;
   }
-  System.Console.WriteLine(num % 10);
+  System.Console.WriteLine($"{initialNumber} -> {num % 10}");
 }
 else
 {
-  System.Console.WriteLine("третьей цифры нет");
+  System.Console.WriteLine($"{initialNumber} -> третьей цифры нет");
 }
