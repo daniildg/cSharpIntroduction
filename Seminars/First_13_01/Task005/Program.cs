8246011f05ad83b2b56e049973d11b00ea267f96
Daniil Grishin <ddg071997@gmail.com>
2023-01-19 21:35:41 +0300

    Added Task005 Multiply Table

---
+++ Seminars/First_13_01/Task005/Program.cs
@@ -0,0 +1,11 @@
+﻿// Необходимо вывести на экран таблицу умножения на 3.
+
+int num = 0;
+int num2 = 1;
+int multiplier = 3;
+while (num2 < 11)
+{
+  num = num + multiplier;
+  Console.WriteLine(multiplier + " * " + num2 + " = " + multiplier * num2);
+  num2++;
+}
