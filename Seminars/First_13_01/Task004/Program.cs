a440aa084d48d7c04585fc84b9b2911e0015b3f4
Daniil Grishin <ddg071997@gmail.com>
2023-01-19 21:21:23 +0300

    Added Task004 Last Digit Number

---
+++ Seminars/First_13_01/Task004/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Enter Number -> ");
+int num = Convert.ToInt32(Console.ReadLine());
+int lastDigit = num % 10;
+System.Console.WriteLine($"{num} -> {lastDigit}");
