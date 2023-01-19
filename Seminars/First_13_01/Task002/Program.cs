95ae8e33605e505c8e60f829d59afa58c70a6a66
Daniil Grishin <ddg071997@gmail.com>
2023-01-19 20:45:21 +0300

    Updated Task002 SwitchCase WeekDay

--- Seminars/First_13_01/Task002/Program.cs
+++ Seminars/First_13_01/Task002/Program.cs
@@ -2,35 +2,37 @@
 
 Console.Write("Enter Number -> ");
 int num = Convert.ToInt32(Console.ReadLine());
-if(num == 1)
+switch(num)
 {
+  case 1:
   System.Console.WriteLine($"{num} -> Понедельник");
-}
-else if(num == 2)
-{
+  break;
+
+  case 2:
   System.Console.WriteLine($"{num} -> Вторник");
-}
-else if(num == 3)
-{
+  break;
+
+  case 3:
   System.Console.WriteLine($"{num} -> Среда");
-}
-else if(num == 4)
-{
+  break;
+
+  case 4:
   System.Console.WriteLine($"{num} -> Четверг");
-}
-else if(num == 5)
-{
+  break;
+
+  case 5:
   System.Console.WriteLine($"{num} -> Пятница");
-}
-else if(num == 6)
-{
+  break;
+
+  case 6:
   System.Console.WriteLine($"{num} -> Суббота");
-}
-else if(num == 7)
-{
+  break;
+
+  case 7:
   System.Console.WriteLine($"{num} -> Воскресенье");
-}
-else
-{
+  break;
+
+  default:
   System.Console.WriteLine($"{num} -> Неизвестное число");
+  break;
 }
