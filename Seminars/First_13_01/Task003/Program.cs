5cc79828839bbfe727d3129b740ab0bbdbbc9384
Daniil Grishin <ddg071997@gmail.com>
2023-01-19 21:11:05 +0300

    Added Task004 Interval

---
+++ Seminars/First_13_01/Task003/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+System.Console.Write("Enter Number -> ");
+int initial_num = Convert.ToInt32(Console.ReadLine());
+int num = initial_num * -1;
+System.Console.Write($"{initial_num} -> ");
+while(num <= initial_num - 1)
+{
+  System.Console.Write(num + ",");
+  num++;
+}
+System.Console.WriteLine(num + ".");
