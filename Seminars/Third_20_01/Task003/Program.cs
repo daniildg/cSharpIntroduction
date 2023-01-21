d9af8f507a9a6a72ae94943227aa0e67992daae1
Daniil Grishin <ddg071997@gmail.com>
2023-01-21 18:37:43 +0300

    Added Task003 Solution

---
+++ Seminars/Third_20_01/Task003/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу квадратов чисел от 1 до N.
+int n = new Random().Next(1, 21);
+System.Console.WriteLine($"num = {n}");
+
+for (int i = 1; i <= n; i++)
+{
+  System.Console.WriteLine($"{i}^2 = {Math.Pow(i, 2)}");
+}
