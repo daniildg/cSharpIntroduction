6649f5cde749d273044ac6d0fabae0945c6f6462
Daniil Grishin <ddg071997@gmail.com>
2023-01-22 00:23:54 +0300

    Added Task023 Solution

---
+++ Homework/HW003-21-01-23/Task023/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+int n = new Random().Next(1, 100);
+System.Console.WriteLine($"num = {n}");
+
+for (int i = 1; i <= n; i++)
+{
+  System.Console.WriteLine($"{i}^3 = {Math.Pow(i, 3)}");
+}
