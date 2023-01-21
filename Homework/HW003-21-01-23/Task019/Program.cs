6b2450658846d5b2273661aeb12188f7f95a21fd
Daniil Grishin <ddg071997@gmail.com>
2023-01-21 23:57:11 +0300

    Added Task019 Solution

---
+++ Homework/HW003-21-01-23/Task019/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+System.Console.Write("Введите число -> ");
+int num = Convert.ToInt32(Console.ReadLine());
+int initial_num = num;
+int result = 0;
+
+while(num > 0)
+{
+  result = (result * 10) + num % 10;
+  num = num / 10;
+}
+if(result == initial_num)
+{
+  System.Console.WriteLine($"{initial_num} -> да");
+}
+else
+{
+  System.Console.WriteLine($"{initial_num} -> нет");
+}
