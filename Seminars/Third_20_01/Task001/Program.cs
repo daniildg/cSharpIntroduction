5438bf3d639d442a319074e2cdbdc0c2a0c2d452
Daniil Grishin <ddg071997@gmail.com>
2023-01-21 19:23:16 +0300

    Added Task001 Solution

---
+++ Seminars/Third_20_01/Task001/Program.cs
@@ -0,0 +1,22 @@
+﻿//Программа, которая по заданному номеру четверти оси координат (x,y) 
+// и показывает диапозон возможных координат точек в этой четверти.
+
+int num = new Random().Next(1,5);
+System.Console.WriteLine(num);
+
+if(num == 1)
+{
+  System.Console.WriteLine("x:= (0; +inf), y:= (0; +inf)");
+}
+else if(num == 2)
+{
+  System.Console.WriteLine("x:= (0; -inf), y:= (0; +inf)");
+}
+else if(num == 3)
+{
+  System.Console.WriteLine("x:= (0; -inf), y:= (0; -inf)");
+}
+else
+{
+  System.Console.WriteLine("x:= (0; +inf), y:= (0; -inf)");
+}
