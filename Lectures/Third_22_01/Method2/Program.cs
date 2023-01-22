0a7cc41eaeef269585b58e7abf707c0a1cd618b5
Daniil Grishin <ddg071997@gmail.com>
2023-01-22 14:52:01 +0300

    Added Method 2 Project

---
+++ Lectures/Third_22_01/Method2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Метод 2
+// принимает, не возвращает
+
+//void Method2(string msg)
+//{
+//  System.Console.WriteLine(msg);
+//}
+//Method2("текст сообщения");
+
+void Method21(string msg, int count)
+{
+  int i = 0;
+  while(i < count)
+  {
+    System.Console.WriteLine(msg);
+    i++;
+  }
+}
+//Method21(msg: "Текст", count: 4);
+Method21(count: 4, msg: "Текст");
