b28d70f841cc2154099a7711a4fc5bed79f34837
Daniil Grishin <ddg071997@gmail.com>
2023-01-22 15:17:08 +0300

    Added Example014 For Cycle Project

---
+++ Lectures/Third_22_01/Example014_For_Cycle/Program.cs
@@ -0,0 +1,16 @@
+﻿//Цикл for
+// на примере Method 4
+
+string Method4(int count, string text)
+{
+  string result = String.Empty;
+
+  for(int i = 0; i < count; i++) // позволяет сократить запись цикла
+  {
+    result = result + text;
+  }
+  return result;
+}
+//string result = Method4(10, "asdf ");
+string result = Method4(text: "asdf ", count: 10);
+//System.Console.WriteLine(result);
