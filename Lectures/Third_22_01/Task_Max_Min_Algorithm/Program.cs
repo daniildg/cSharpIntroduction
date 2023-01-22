2664afcba038b8d3f13d808d2520aa9b83b8dc72
Daniil Grishin <ddg071997@gmail.com>
2023-01-22 18:10:35 +0300

    Added Task Max To Min Solution

---
+++ Lectures/Third_22_01/Task_Max_Min_Algorithm/Program.cs
@@ -0,0 +1,35 @@
+﻿//Распределить числа в массиве от большего к меньшему
+// по приниципу сортировки методом выбора
+
+int[] arr = {1,5,4,3,2,6,7,1,1};
+
+void printArray(int[]array)
+{
+  int count = array.Length;
+  
+  for(int i = 0; i < count; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+
+void selectionSort(int[] array)
+{
+  for(int i = 0; i < array.Length - 1; i++)
+  {
+    int maxPosition = i;
+    
+    for(int j = i + 1; j < array.Length - 2; j++)           
+    {                                                   
+      if(array[j] > array[maxPosition]) maxPosition =j; 
+    }                                                   
+    //
+    int temp = array[i];
+    array[i] = array[maxPosition];
+    array[maxPosition] = temp; //обмен двух пременнных местами
+  }
+}
+printArray(arr);
+selectionSort(arr);
+printArray(arr);
