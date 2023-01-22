8c58e1e31c51f63fa1a88263ad59896e6a3392da
Daniil Grishin <ddg071997@gmail.com>
2023-01-22 15:05:29 +0300

    Added Method 4 Project

---
+++ Lectures/Third_22_01/Method4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Метод 4
+// принимает, возвращает
+
+string Method4(int count, string c)
+{
+  int i = 0;
+  string result = String.Empty;
+
+  while(i < count)
+  {
+    result = result + c;
+    i++;
+  }
+  return result;
+}
+string result = Method4(10, "asdf ");
+System.Console.WriteLine(result);
