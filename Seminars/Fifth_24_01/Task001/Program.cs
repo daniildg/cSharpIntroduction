a00a20801b1f73d78e7d4e776a921a262e631740
Daniil Grishin <ddg071997@gmail.com>
2023-01-24 20:21:22 +0300

    Added Task001 Solution

---
+++ Seminars/Fifth_24_01/Task001/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+static int GetSum(int num)
+{
+  int res = 0;
+  for(int i = 1; i <= num; i++)
+  {
+    res += i;
+  }
+  return res;
+}
+int num = 8;
+System.Console.WriteLine(GetSum(num));
