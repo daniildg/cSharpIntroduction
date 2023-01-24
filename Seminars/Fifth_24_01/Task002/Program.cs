db3272500acf1d7c1e415d6528f4ff60f4542b62
Daniil Grishin <ddg071997@gmail.com>
2023-01-24 21:59:14 +0300

    Added Task002 Solution

---
+++ Seminars/Fifth_24_01/Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+static int GetCountNum(int num)
+{
+  System.Console.WriteLine(num);
+  int count = 0;
+  while(num > 0)
+  {
+    count++;
+    num /= 10;
+  }
+  return count;
+}
+System.Console.WriteLine(GetCountNum(new Random().Next(1000, 100000)));
