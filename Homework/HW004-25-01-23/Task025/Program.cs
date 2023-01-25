4b4b6c9272b25920e8d7ef90a9cf891a4fb0deb3
Daniil Grishin <ddg071997@gmail.com>
2023-01-25 20:05:27 +0300

    Added Task025 Solution

---
+++ Homework/HW004-25-01-23/Task025/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+int numberA = new Random().Next(1,11);
+int numberB = new Random().Next(1,6);
+
+static int Exponent(int numA, int numB)
+{
+  int result = 1;
+  for(int ind = 0; ind < numB; ind++)
+  {
+    result = result * numA;
+  }
+  return result;
+}
+System.Console.WriteLine($"{numberA} ^ {numberB} -> {Exponent(numberA, numberB)}");
