95d87622131944c5df7d91cc4e6b04579150e4e0
Daniil Grishin <ddg071997@gmail.com>
2023-01-25 21:07:01 +0300

    Added Task027 Solution

---
+++ Homework/HW004-25-01-23/Task027/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+static int DigitSum(int num)
+{
+  int sum = 0;
+  for (int n = num; num > 0; num /= 10)
+  {
+    sum = num % 10 + sum;
+  }
+  return sum;
+}
+int number = new Random().Next(100, 10000);
+System.Console.WriteLine($"{number} -> {DigitSum(number)}");
