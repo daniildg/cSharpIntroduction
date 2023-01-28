4b7ffb2b759abff5e4cf0c65ba74f2a8940839b1
Daniil Grishin <ddg071997@gmail.com>
2023-01-28 11:23:47 +0300

    Added Task001 Solution

---
+++ Seminars/Sixth_27_01/Task001/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine();
+Console.WriteLine($"{GetSumMoreZero(arr)}, {GetSumLessZero(arr)}");
+int GetSumMoreZero(int[] array)
+{
+  int result = 0;
+  for(int i = 0; i < array.Length; i++)
+  {
+    if(array[i] >= 0)
+    {
+      result += arr[i];
+    }
+  }
+  return result;
+}
+
+int GetSumLessZero(int[] array)
+{
+  int res = 0;
+  for(int i = 0; i < array.Length; i++)
+  {
+    if(array[i] < 0)
+    {
+      res += arr[i];
+    }
+  }
+  return res;
+}
+
+int[] GetRandomArray(int length)
+{
+  int[] result = new int[length];
+
+  for(int i = 0; i < length; i++)
+  {
+    result[i] = new Random().Next(-100, 100);
+  }
+  return result;
+}
+
+void PrintArray(int[] arr)
+{
+  foreach(int item in arr)
+  {
+    Console.Write(item + " ");
+  }
+}
