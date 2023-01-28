160f428ca2b643ab3f9a3d743a54bbfd7bc9b234
Daniil Grishin <ddg071997@gmail.com>
2023-01-28 13:31:45 +0300

    Added Task003 Solution

---
+++ Seminars/Sixth_27_01/Task003/Program.cs
@@ -0,0 +1,36 @@
+﻿// // Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine(FindNum(10, arr));
+
+bool FindNum(int num, int[] array)
+{
+  for(int i = 0; i < array.Length; i++)
+	{
+		if(array[i] == num) return true;
+  }
+    return false;
+}
+
+int[] GetRandomArray(int length)
+{
+  int[] result = new int[length];
+
+  for(int i = 0; i < length; i++)
+  {
+    result[i] = new Random().Next(0, 15);
+  }
+  return result;
+}
+
+void PrintArray(int[] arr)
+{
+  foreach(int item in arr)
+  {
+    Console.Write(item + " ");
+  }
+  System.Console.WriteLine();
+}
