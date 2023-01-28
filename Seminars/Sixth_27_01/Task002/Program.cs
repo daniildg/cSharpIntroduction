842c59bfbafccc8257369f0299d21fd960ab8b89
Daniil Grishin <ddg071997@gmail.com>
2023-01-28 11:26:28 +0300

    Added Task002 Solution

---
+++ Seminars/Sixth_27_01/Task002/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу замена элементов массива: положительные элементы замените 
+//на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine();
+PrintArray(ReplaceNum(arr));
+
+int[] ReplaceNum(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+int[] GetRandomArray(int length)
+{
+  int[] result = new int[length];
+
+  for(int i = 0; i < length; i++)
+  {
+    result[i] = new Random().Next(-100, 100);
+  }
+  return result;
+}
+
+void PrintArray(int[] arr)
+{
+  foreach(int item in arr)
+  {
+    Console.Write(item + " ");
+  }
+}
