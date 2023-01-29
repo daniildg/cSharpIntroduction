0cbf943fc59b15cb08be8bbf11f58d84dd20150b
Daniil Grishin <ddg071997@gmail.com>
2023-01-29 17:40:59 +0300

    Added Task005 Solution

---
+++ Seminars/Sixth_27_01/Task005/Program.cs
@@ -0,0 +1,53 @@
+﻿//Найдите произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, 
+//второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+int[] arr = GetRandomArray(6);
+PrintArray(arr);
+int[] newArray = FindSumOfPairs(arr);
+PrintArray(newArray);
+
+int[] FindSumOfPairs(int[] array)
+{
+    int newSize = array.Length / 2;
+
+    if(array.Length % 2 == 1)
+    {
+        newSize++;
+    }
+
+    int[] result = new int[newSize];
+
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length -1 - i];
+    }
+
+    if(array.Length % 2 == 1)
+    {
+        result[newSize - 1] = array[array.Length / 2];
+    }
+    return result;
+}
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(1, 11);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
