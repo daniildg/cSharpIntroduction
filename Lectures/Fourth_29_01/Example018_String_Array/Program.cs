7cb866d66efd3109a1260a9e803412de5cb0ff00
Daniil Grishin <ddg071997@gmail.com>
2023-02-01 22:00:41 +0300

    Added Example018 String Array Project

---
+++ Lectures/Fourth_29_01/Example018_String_Array/Program.cs
@@ -0,0 +1,12 @@
+﻿// Двумерный массив
+
+string[,] table = new string[2, 5]; // массив из строк
+table[1, 2] = "hello";
+for(int rows = 0; rows < 2; rows++)
+{
+   for (int columns = 0; columns < 3; columns++)
+   { 
+       Console.WriteLine($"-{table[rows, columns]}-"); 
+   }
+}
+Console.WriteLine();
