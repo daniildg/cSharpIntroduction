5fcc73cc5a722d1c15d10c055bb1d6afb72a77fc
Daniil Grishin <ddg071997@gmail.com>
2023-02-01 22:33:49 +0300

    Added Project023 Fibonacci Recursion Project

---
+++ Lectures/Fourth_29_01/Example023_Fibonacci_Recursion/Program.cs
@@ -0,0 +1,13 @@
+﻿// Рекурсия для числа Фибоначчи
+
+double Fibonacci(int n)
+{
+   if (n==0) return 0;
+   if (n == 1 || n == 2) return 1;
+   else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 0; i < 50; i++)
+{
+   Console.WriteLine($"f{i} = {Fibonacci(i)}");
+}
