379093e7c2be369d79bd658b82b9905a46ea7b77
Daniil Grishin <ddg071997@gmail.com>
2023-02-01 22:25:51 +0300

    Added Example022 Factorial Recursion Project

---
+++ Lectures/Fourth_29_01/Example022_Factorial_Recursion/Program.cs
@@ -0,0 +1,12 @@
+﻿// Рекурсия для факториала
+
+double Factorial(int n)
+{
+   if(n == 1) return 1;
+   else return n * Factorial(n - 1);
+}
+
+for(int i = 1; i < 40; i++)
+{
+   Console.WriteLine($"{i}! = {Factorial(i)}");
+}
