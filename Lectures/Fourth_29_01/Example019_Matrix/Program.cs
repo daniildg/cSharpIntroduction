a5b072c901cf831545555a976803be1c98b5ab05
Daniil Grishin <ddg071997@gmail.com>
2023-02-01 22:04:11 +0300

    Added Example019 Matrix Project

---
+++ Lectures/Fourth_29_01/Example019_Matrix/Program.cs
@@ -0,0 +1,9 @@
+﻿int[,] matrix = new int[3, 4]; // прямоугольная таблица чисел (матрица)
+for(int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for(int columns =0; columns < matrix.GetLength(1); columns++)
+    {
+        Console.Write($"{matrix[rows, columns]} ");
+    }
+    Console.WriteLine();
+}
