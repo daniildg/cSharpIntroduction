aee2498a30eeb855902b2688868bfdca19bedf88
Daniil Grishin <ddg071997@gmail.com>
2023-02-01 21:57:38 +0300

    Updated Task038 - "Array.Sort Method"

--- Homework/HW005-29-01-23/Task038/Program.cs
+++ Homework/HW005-29-01-23/Task038/Program.cs
@@ -3,45 +3,50 @@
 //[3 7 22 2 78] -> 76
 
 int[] array = GetRandomArray(10);
-PrintArray(array);
-Console.WriteLine($"{Max(array)} - {Min(array)} = {Max(array) - Min(array)}");
-
-int Max(int[] array)
+Array.Sort(array);
+foreach (int item in array)
 {
-    int result = array[0];
-    for(int i = 1; i < array.Length; i++)
-    {
-        if (array[i] > result) result = array[i];
-    }
-    return result;
+  Console.Write(item + " ");
 }
-
-int Min(int[] array)
-{
-    int result = array[0];
-    for (int ind = 1; ind < array.Length; ind++)
-    {
-        if (array[ind] < result) result = array[ind];
-    }
-    return result;
-}
+Console.WriteLine();
+System.Console.WriteLine($"{array[9]} - {array[0]} = {array[9] - array[0]}");
 
 int[] GetRandomArray(int length)
 {
-    int[] result = new int[length];
+  int[] result = new int[length];
 
-    for (int i = 0; i < length; i++)
-    {
-        result[i] = new Random().Next(0, 101);
-    }
-    return result;
+  for (int i = 0; i < length; i++)
+  {
+    result[i] = new Random().Next(0, 101);
+  }
+  return result;
 }
 
-void PrintArray(int[] arr)
-{
-    foreach (int item in arr)
-    {
-        Console.Write(item + " ");
-    }
-    Console.WriteLine();
-}
+// int Max(int[] array)
+// {
+//     int result = array[0];
+//     for(int i = 1; i < array.Length; i++)
+//     {
+//         if (array[i] > result) result = array[i];
+//     }
+//     return result;
+// }
+
+// int Min(int[] array)
+// {
+//     int result = array[0];
+//     for (int ind = 1; ind < array.Length; ind++)
+//     {
+//         if (array[ind] < result) result = array[ind];
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     foreach (int item in arr)
+//     {
+//         Console.Write(item + " ");
+//     }
+//     Console.WriteLine();
+// }
