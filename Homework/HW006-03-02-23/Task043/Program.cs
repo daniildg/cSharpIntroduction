76fda445c60353dc10bdb8ed4f220569b8e992d5
Daniil Grishin <ddg071997@gmail.com>
2023-02-03 17:38:56 +0300

    Added Task043 Solution

---
+++ Homework/HW006-03-02-23/Task043/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+float b1 = new Random().Next(-25, 25); Console.Write($"b1 = {b1} ");
+float k1 = new Random().Next(-25, 25); Console.Write($"k1 = {k1} ");
+float b2 = new Random().Next(-25, 25); Console.Write($"b2 = {b2} ");
+float k2 = new Random().Next(-25, 25); Console.Write($"k2 = {k2}; ");
+float x = (b2 - b1) / (k1 - k2);
+Console.WriteLine($"y = ({(k1 * x) + b1}; {(k2 * x) + b2})");
