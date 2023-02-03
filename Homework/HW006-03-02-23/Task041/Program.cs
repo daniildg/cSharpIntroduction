855344e2eda4269dde8226035d37fd6486fcb4db
Daniil Grishin <ddg071997@gmail.com>
2023-02-03 17:19:40 +0300

    Added Task041 Solution

---
+++ Homework/HW006-03-02-23/Task041/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите ваши числа через пробел -> ");
+string line = Console.ReadLine();
+string[] sep = line.Split(' ');
+int[] convert = Array.ConvertAll<string, int>(sep, int.Parse);
+Console.WriteLine($"Чисел больше нуля -> {MoreThanZero(convert)}");
+
+int MoreThanZero(int[] array)
+{
+  int res = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0) res++;
+  }
+  return res;
+}
