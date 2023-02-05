afa602511971270ea355aac7a865fd3b3772ee98
Daniil Grishin <ddg071997@gmail.com>
2023-02-05 19:52:37 +0300

    Added Task004 Solution

---
+++ Seminars/Seventh_03_02/Task004/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 51: Задайте двумерный массив. 
+Найдите сумму элементов, находящихся на главной диагонали 
+(с индексами (0,0); (1;1) и т.д.
+*/
+
+Print2DArray(Generate2DArray(3, 4));
+
+
+
+int[,] Generate2DArray(int m, int n)
+{
+   int[,] result = new int[m, n];
+   for(int i = 0; i < result.GetLength(0); i++)
+   {
+       for(int j = 0; j < result.GetLength(1); j++)
+       {
+           result[i, j] = new Random().Next(1, 100);
+       }
+   }
+   return result;
+}
+
+void Print2DArray(int[,] array)
+{  
+   for (int i = 0; i < arr.GetLength(0); i++)
+   {
+       for (int j = 0; j < arr.GetLength(1); j++)
+       {
+           Console.Write($"{arr[i, j]}");
+       }
+   }
+}
