be8928cb07f94e8585fb446694a24e41ede6ae62
Daniil Grishin <ddg071997@gmail.com>
2023-02-05 20:08:41 +0300

    Updated Task004 Solution

--- Seminars/Seventh_03_02/Task004/Program.cs
+++ Seminars/Seventh_03_02/Task004/Program.cs
@@ -3,30 +3,43 @@
 (с индексами (0,0); (1;1) и т.д.
 */
 
-Print2DArray(Generate2DArray(3, 4));
+var arr = Generate2DArray(4, 4);
+Print2DArray(arr);
+Console.WriteLine();
+System.Console.WriteLine(GetSum(arr));
 
-
+int GetSum(int[,] array)
+{
+  int length = array.GetLength(0) < array.GetLength(1) ? array.GetLength(0) : array.GetLength(1);
+  int sum = 0;
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    sum += array[i, i];
+  }
+  return sum;
+}
 
 int[,] Generate2DArray(int m, int n)
 {
-   int[,] result = new int[m, n];
-   for(int i = 0; i < result.GetLength(0); i++)
-   {
-       for(int j = 0; j < result.GetLength(1); j++)
-       {
-           result[i, j] = new Random().Next(1, 100);
-       }
-   }
-   return result;
+  int[,] result = new int[m, n];
+  for (int i = 0; i < result.GetLength(0); i++)
+  {
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+      result[i, j] = new Random().Next(1, 100);
+    }
+  }
+  return result;
 }
 
 void Print2DArray(int[,] array)
-{  
-   for (int i = 0; i < arr.GetLength(0); i++)
-   {
-       for (int j = 0; j < arr.GetLength(1); j++)
-       {
-           Console.Write($"{arr[i, j]}");
-       }
-   }
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write($"{arr[i, j]} ");
+    }
+    Console.WriteLine();
+  }
 }
