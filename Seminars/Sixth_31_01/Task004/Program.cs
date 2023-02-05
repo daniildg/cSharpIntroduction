6e0059de3db85fbb385b0572c7c75d4080877544
Daniil Grishin <ddg071997@gmail.com>
2023-02-05 14:42:10 +0300

    Added Task004 Solution

---
+++ Seminars/Sixth_31_01/Task004/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 45: Напишите программу, 
+которая будет создавать копию заданного массива 
+с помощью поэлементного копирования.
+*/
+int[] array = GetRandomArray(10);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(CopyArray(array));
+
+int[] CopyArray(int[] arr)
+{
+  int[] result = new int[arr.Length];
+  for(int i = 0; i < arr.Length; i++)
+  {
+    result[i] = arr[i];
+  }
+  return result;
+}
+
+int[] GetRandomArray(int length)
+{
+  int[] result = new int[length];
+
+  for(int i = 0; i < length; i++)
+  {
+    result[i] = new Random().Next(-100, 100);
+  }
+  return result;
+}
+
+void PrintArray(int[] arr)
+{
+  foreach(int item in arr)
+  {
+    Console.Write(item + " ");
+  }
+}
