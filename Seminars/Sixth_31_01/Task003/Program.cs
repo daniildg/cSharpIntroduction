eb2108d9371b3385d284395d1e72b1034ed66e03
Daniil Grishin <ddg071997@gmail.com>
2023-02-05 14:23:48 +0300

    Added Task003 Solution

---
+++ Seminars/Sixth_31_01/Task003/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+*/
+
+int fstNum = 0;
+int scdNum = 1;
+int n = new Random().Next(3, 15);
+Console.Write($"{n} -> {fstNum} {scdNum} ");
+for(int i = 3; i <= n; i++)
+{
+  int fibNum = fstNum + scdNum; 
+  Console.Write(fibNum + " ");
+  fstNum = scdNum;
+  scdNum = fibNum;
+}
