5be039a0c52b147f9a938bb7d8e01f4b9986bddf
Daniil Grishin <ddg071997@gmail.com>
2023-02-05 14:11:00 +0300

    Added Task002 Solution

---
+++ Seminars/Sixth_31_01/Task002/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 40: Напишите программу, 
+которая принимает на вход три числа и проверяет, 
+может ли существовать треугольник с сторонами такой длины.
+*/
+
+bool CompareNum(int num1, int num2, int num3)
+{
+  return(((num1 + num2) > num3) 
+        && ((num1 + num3) > num2) 
+        && ((num2 + num3) > num1));
+}
+Console.WriteLine(CompareNum(2, 2, 2));
