a6a33662d2c4d9dca51fd47d125a80433db8a06b
Daniil Grishin <ddg071997@gmail.com>
2023-02-07 18:43:46 +0300

    Updated Task002 - "shortened method"

--- Homework/HW007-05-02-23/Task002/Program.cs
+++ Homework/HW007-05-02-23/Task002/Program.cs
@@ -6,8 +6,8 @@
 8 4 2 4
 17 -> такого числа в массиве нет*/
 
-int m = new Random().Next(0,6);
-int n = new Random().Next(0,6);
+int m = new Random().Next(0,5);
+int n = new Random().Next(0,5);
 Console.WriteLine($"Координаты -> {m}, {n}");
 var array = CreateMatrix(3, 4);
 PrintMatrix(array);
@@ -15,17 +15,7 @@ Console.WriteLine($"{GetResult(array, m, n)}");
 
 string GetResult(int[,] arr, int m, int n)
 {
-  if (m < arr.GetLength(0) && n < arr.GetLength(1))
-  {
-    for (int k = 0; k < arr.GetLength(0); k++)
-    {
-      for (int l = 0; l < arr.GetLength(1); l++)
-      {
-        if (arr[k, l] == arr[m, n]) return $"{arr[k, l]} -> это число есть";
-      }
-    }
-    return "  -> такого числа нет";
-  }
+  if (m < arr.GetLength(0) && n < arr.GetLength(1)) return $"{arr[m, n]} -> это число есть";
   else return "  -> такого числа нет ";
 }
 
