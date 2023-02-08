6d09fd143c18a5200c3136b09de0c93063fc154f
Daniil Grishin <ddg071997@gmail.com>
2023-02-08 21:37:17 +0300

    Added Task003 Solution

---
+++ Seminars/Eighth_07_02/Task003/Program.cs
@@ -0,0 +1,74 @@
+﻿/* Задача 57: Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных. */
+
+int[,] matrix = CreateMatrix(4, 4);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[] array = GetArray(matrix);
+Array.Sort(array);
+WriteVocabulary(array);
+
+void WriteVocabulary(int[] arr)
+{
+  int element = arr[0];
+  int count = 1;
+  for(int i = 1; i < arr.Length; i++)
+  {
+    if(element != array[i])
+    {
+      Console.WriteLine($"{element} -> {count} раз(а)");
+      element = array[i];
+      count = 1;
+    }
+    else count++;
+  }
+  Console.WriteLine($"{element} -> {count} раз(а)");
+}
+
+void PrintArray(int[] arr)
+{
+  for(int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+}
+
+int[] GetArray(int [,] matr)
+{
+  int[] result = new int[matr.GetLength(0) * matr.GetLength(1)];
+  int index = 0;
+  for(int i = 0; i < matr.GetLength(0); i++)
+  {
+    for(int j = 0; j < matr.GetLength(1); j++)
+    {
+      result[index] = matr[i, j];
+      index++;
+    }
+  }
+  return result;
+}
+
+void PrintMatrix(int[,] arr)
+{
+  for (int k = 0; k < arr.GetLength(0); k++)
+  {
+    for (int l = 0; l < arr.GetLength(1); l++)
+    {
+      Console.Write($"{arr[k, l]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] CreateMatrix(int m, int n)
+{
+  int[,] matr = new int[m, n];
+  for (int k = 0; k < matr.GetLength(0); k++)
+  {
+    for (int l = 0; l < matr.GetLength(1); l++)
+    {
+      matr[k, l] = new Random().Next(1, 11);
+    }
+  }
+  return matr;
+}
