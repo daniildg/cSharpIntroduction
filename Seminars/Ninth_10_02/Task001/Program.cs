e186b681b5fa3703689c67b62f307bb778a8f0c2
Daniil Grishin <ddg071997@gmail.com>
2023-02-13 18:37:14 +0300

    Added Task001 Solution

---
+++ Seminars/Ninth_10_02/Task001/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+
+string NumbersRec(int n)
+{
+    if (n == 1) return $"{1} ";
+    else return NumbersRec(n - 1) + n + " ";
+}
+
+Console.WriteLine(NumbersRec(5));
