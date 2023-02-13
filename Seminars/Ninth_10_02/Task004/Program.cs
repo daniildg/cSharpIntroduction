fa62828d17a7ace8bab647ff32b67a2b3731c2b9
Daniil Grishin <ddg071997@gmail.com>
2023-02-13 18:56:14 +0300

    Added Task004 Solution

---
+++ Seminars/Ninth_10_02/Task004/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 */
+
+int Pow(int number, int rang)
+{
+    if (rang == 0)
+    {
+        return 1;
+    }
+    if (rang == 1)
+    {
+        return number;
+    }
+    return number * Pow(number, rang - 1);
+}
+
+System.Console.WriteLine(Pow(3, 5));
