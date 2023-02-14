b3b565a2468a7c7a19996ace103d75de90795ec8
Daniil Grishin <ddg071997@gmail.com>
2023-02-14 18:17:15 +0300

    Updeted Task005 - "fixing random array"

--- Homework/HW008-08-02-23/Task004/Program.cs
+++ Homework/HW008-08-02-23/Task004/Program.cs
@@ -2,8 +2,8 @@
 Напишите программу, которая будет построчно выводить массив,
 добавляя индексы каждого элемента. */
 
-int[,,] cubeArray = Create3DArray(3, 3, 3);
-Write3DArray(cubeArray);
+int[,,] array = Create3DArray(3, 3, 3);
+Write3DArray(array);
 
 void Write3DArray(int[,,] cube)
 {
@@ -23,33 +23,33 @@ void Write3DArray(int[,,] cube)
 
 int[,,] Create3DArray(int a, int b, int c)
 {
-    int[,,] cubeArray = new int[a, b, c];
-    cubeArray[0, 0, 0] = new Random().Next(10, 100);
-    for (int i = 0; i < cubeArray.GetLength(0); i++)
+  int[,,] cubeArray = new int[a, b, c];
+  for (int i = 0; i < cubeArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < cubeArray.GetLength(1); j++)
     {
-        for (int j = 0; j < cubeArray.GetLength(1); j++)
-        {
-            for (int k = 0; k < cubeArray.GetLength(2); k++)
-            {
-                cubeArray[i, j, k] = IsContained(cubeArray);
-            }
-        }
+      for (int k = 0; k < cubeArray.GetLength(2); k++)
+      {
+        int num = new Random().Next(10, 100);
+        if(IsNotContained(cubeArray, num) != true)
+        else cubeArray[i, j, k] = num;
+      }
     }
-    return cubeArray;
+  }
+  return cubeArray;
 }
 
-int IsContained(int[,,] cube)
+bool IsNotContained(int[,,] cube, int el)
 {
-    int i = 0;
-    int length = cube.GetLength(0)
-        * cube.GetLength(1) * cube.GetLength(2);
-    int el = cube[0, 0, 0];
-    while (i < length)
+for (int i = 0; i < cube.GetLength(0); i++)
+  {
+    for (int j = 0; j < cube.GetLength(1); j++)
     {
-        int newEl = new Random().Next(10, 100);
-        if (newEl != el) return newEl;
-        el = newEl;
-        i++;
+      for (int k = 0; k < cube.GetLength(2); k++)
+      {
+        if(el == cube[i, j, k]) return false;
+      }
     }
-    return el;
+  }
+  return true;
 }
