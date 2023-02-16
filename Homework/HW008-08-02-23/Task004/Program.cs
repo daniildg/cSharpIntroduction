ee84391dfa0fec1cc90b57f8d147022fb4a1a22a
Daniil Grishin <ddg071997@gmail.com>
2023-02-16 09:18:29 +0300

    Updated Task005 - "keep workin on unique array"

--- Homework/HW008-08-02-23/Task004/Program.cs
+++ Homework/HW008-08-02-23/Task004/Program.cs
@@ -30,9 +30,18 @@ int[,,] Create3DArray(int a, int b, int c)
     {
       for (int k = 0; k < cubeArray.GetLength(2); k++)
       {
+        int cubeLength = cubeArray.GetLength(0)
+                      * cubeArray.GetLength(1) * cubeArray.GetLength(2);
         int num = new Random().Next(10, 100);
-        if(IsNotContained(cubeArray, num) != true)
-        else cubeArray[i, j, k] = num;
+
+        if (IsNotContained(cubeArray, num) == true) cubeArray[i, j, k] = num;
+        else
+          for (int ind = 0; ind < cubeLength; ind++)
+          {
+            num = new Random().Next(10, 100);
+            if (IsNotContained(cubeArray, num) == true) break;
+          }
+        cubeArray[i, j, k] = num;
       }
     }
   }
@@ -41,13 +50,13 @@ int[,,] Create3DArray(int a, int b, int c)
 
 bool IsNotContained(int[,,] cube, int el)
 {
-for (int i = 0; i < cube.GetLength(0); i++)
+  for (int i = 0; i < cube.GetLength(0); i++)
   {
     for (int j = 0; j < cube.GetLength(1); j++)
     {
       for (int k = 0; k < cube.GetLength(2); k++)
       {
-        if(el == cube[i, j, k]) return false;
+        if (el == cube[i, j, k]) return false;
       }
     }
   }
